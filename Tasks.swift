2c7948dabf79403c5add44789e374adcce893a04
shelagh-mcgowan <shel4gh@gmail.com>
2015-03-15 18:49:50 -0400

    second commit

---
+++ Tasks.swift
@@ -0,0 +1,18 @@
+//
+//  Tasks.swift
+//  TaskManager
+//
+//  Created by Ravi Shankar on 12/07/14.
+//  Copyright (c) 2014 Ravi Shankar. All rights reserved.
+//b
+
+
+import Foundation
+import CoreData
+
+class Tasks: NSManagedObject {
+
+    @NSManaged var desc: String
+    @NSManaged var desc2: String
+    
+}
